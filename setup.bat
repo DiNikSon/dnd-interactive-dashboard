4b04d1ec7eb1722813ff08358f3f0999bdda7f3b
Vlad Popov <vvladislavv2002@gmail.com>
2026-03-14 13:33:21 +0300

    setup 4

--- setup.bat
+++ setup.bat
@@ -3,7 +3,7 @@
 :: ── Авто-повышение до администратора ─────────────────────────────────────────
 >nul 2>&1 "%SYSTEMROOT%\system32\cacls.exe" "%SYSTEMROOT%\system32\config\system"
 if '%errorlevel%' NEQ '0' (
-    echo Set UAC = CreateObject("Shell.Application") > "%temp%\dndi_elevate.vbs"
+    echo Set UAC = CreateObject^("Shell.Application"^) > "%temp%\dndi_elevate.vbs"
     echo UAC.ShellExecute "%~f0", "", "%~dp0", "runas", 1 >> "%temp%\dndi_elevate.vbs"
     "%temp%\dndi_elevate.vbs"
     del "%temp%\dndi_elevate.vbs" >nul 2>&1
